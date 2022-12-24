267ef8d7fbfafade39cc457555865f914e0258bc
johan <johan@majo.pw>
2022-12-24 08:24:03 +0100

    xujdhkxkjnht

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
+import { url } from 'inspector';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+interface Afbeelding {
+  name: string,
+  path: string,
+}
+
+interface Kavel {
+  nummer: string,
+  name: string,
+  omschrijving: string,
+  afbeeldingen: Afbeelding[],
+};
+
+const kavels: Kavel[] = [
+  {
+    nummer: '01',
+    name: 'Ritje in Ferrari',
+    omschrijving: 'Offroaden voor gevorderden!',
+    afbeeldingen: [
+      {
+        name: 'Ferrari',
+        path: 'auto0.jpg',
+      }, {
+        name: 'Ferrari binnen',
+        path: 'auto1.jpg',
+      }, {
+        name: 'Ferrari nog een',
+        path: 'auto2.jpg',
+      },
+    ]
+  },
+  {
+    nummer: '02',
+    name: 'Ritje in mijn Ford',
+    omschrijving: '<p>Geheel verzorgt<p>Inclusief: <ul><li>Eindschoonmaak</li><li>Beginschoonmaak</li></ul>',
+    afbeeldingen: [
+      {
+        name: 'Ford',
+        path: 'ford.jpeg',
+      }
+    ]
+  },
+];
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{maxWidth: '100vw'}}>
+      {kavels.map((kavel) => (
+        <div style={{display: 'flex', flexDirection: 'column'}} key={kavel.nummer}>
+          <h2>{kavel.name}</h2>
+          <div>{kavel.omschrijving}</div>
+          <div style={{display: 'flex'}}>
+            {kavel.afbeeldingen.map((afbeelding) => (
+                <div style={{backgroundImage: `url('/images/${afbeelding.path}')`, width: '50px', height: '80px'}}></div>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
