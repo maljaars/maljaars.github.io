82239c1e9b00450a9bd0707a960ec1f29441df01
johan <johan@majo.pw>
2023-02-22 18:51:52 +0100

    content

--- src/Components/Intro/Intro.tsx
+++ src/Components/Intro/Intro.tsx
@@ -6,12 +6,18 @@ export const Intro = () => {
       <div className='intro'>
         <div className='intro__content'>
           <h1>Welkom</h1>
-          <p>Welkom op de benefiet veiling in het CSW in Middelburg.</p>
-          <p>Wij zijn Annemieke en Ruud Maljaars uit Middelburg en in oktober 2023 gaan we 63 kilometer wandelen voor Compassion. Met de opbrengst van deze benefiet veiling bevrijden wij kinderen uit extreme armoede en geven zo een hoopvolle toekomst door ze te voorzien van eten, verzorging, medicijnen en onderwijs samen met hun plaatselijke kerk. </p>
-          <p>Samen met Compassion bezoeken we twee dorpen in Ghana o.a. Kintampo. Hier hopen we onze sponsorkinderen Christabel (2 jaar) en Fantinvel (4 jaar) ontmoeten. </p>
-          <p>Wil jij ook een kindje sponsoren? Dat kan voor €33,- per maand. We helpen je graag verder!</p>
-          <p>De veiling vind plaats op 11 maart 2023 om half 15u30 in het oude CSW gebouw aan de Sir Winston Churchilllaan 8. Ingang via de achteringang aan de Koos Vorrinkstraat. Inloop vanaf 15u15.</p>
-          <p>Naast deze veilingstukken is er ook een kraam met leuke cadeautjes die te koop zijn.</p>
+          <p>Welkom op onze benefiet veiling in het CSW in Middelburg.</p>
+          <p>Wij zijn Annemieke en Ruud Maljaars uit Middelburg en in oktober 2023 gaan we 63 kilometer wandelen voor de christelijke hulporganisatie Compassion. Met de opbrengst bevrijden we kinderen uit extreme armoede. Door de plaatselijke kerk horen ze over Jezus en worden ze voorzien van eten, verzorging, medicijnen en onderwijs.</p>
+          <p>
+            <b>MELD JE AAN</b> met de rode aanmeldknop. Dat is voor ons handig in verband met de catering.
+          </p>
+          <p>De veiling vindt plaats op 11 maart 2023 om half 15u30 in het oude CSW gebouw aan de Sir Winston Churchilllaan 8. Ingang via de achteringang aan de Koos Vorrinkstraat. Inloop vanaf 15u15.</p>
+          <p>
+            Naast deze veilingstukken is er ook een kraam met leuke cadeautjes die te koop zijn.
+          </p>
+          <p>
+            Compassion heeft de ANBI-status.
+          </p>
         </div>
         <div className='intro__graphic'>
           <img className='intro__graphic--image' src="/images/intro.jpg" alt="Foto van de lopers" />
@@ -19,7 +25,7 @@ export const Intro = () => {
       </div>
       <div className='intro'>
         <div className='intro__content'>
-        Aanmelden voor de veiling kan via de onderstaande button
+          Aanmelden voor de veiling kan via de onderstaande button
           <div className='buttons'>
             <a href='https://docs.google.com/forms/d/e/1FAIpQLSfdNKv85vB6Q97jyRmzEQoGh63vIcHRHdaDQzZ9ZvrhamBRnQ/viewform' target='_blank' className='button'>Aanmelden</a>
           </div>
